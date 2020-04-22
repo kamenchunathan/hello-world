32aa96e570efe0f6d68909627af469341cdae75c
nat3st <48732508+nat3st@users.noreply.github.com>
2020-04-22 15:03:53 +0300

    Create helloworld.cs

---
+++ helloworld.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace helloworld{
+
+  class program{
+      
+      public void main(string[], args){
+        Console.WriteLine(" Hello World! \nW'ell get there. Baby steps");
+      }
+  }
+}
